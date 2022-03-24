896f00e539adc1e081aacbec5267a9a381bed955
nida.ghuman <nidaghuman@nidas-mbp.myfiosgateway.com>
2022-03-24 11:43:45 -0400

    added article tables

---
+++ db/migrate/20220324153647_update_articles.rb
@@ -0,0 +1,7 @@
+class UpdateArticles < ActiveRecord::Migration[5.2]
+  def change
+    add_column :articles, :description, :text
+    add_column :articles, :created_at, :datetime
+    add_column :articles, :updated_at, :datetime
+  end
+end
