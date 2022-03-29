fb5b675481e340c3c3abf54a717ac69cc78a5337
nida.ghuman <nidaghuman@nidas-mbp.myfiosgateway.com>
2022-03-29 09:16:00 -0400

    added associations between user and article tables

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 #every new class will inheret from ApplicationRecord which is what that syntax means
 class Article < ApplicationRecord
+belongs_to :user
 validates :title, presence: true, length: {minimum: 2, maximun: 100}
 validates :description, presence: true, length: {minimum: 10, maximum: 300}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_29_122225) do
+ActiveRecord::Schema.define(version: 2022_03_29_125947) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
