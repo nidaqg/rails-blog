09e5ad7e28c521bf2fb8fdb70b2d3017a817656b
nida.ghuman <nidaghuman@nidas-mbp.myfiosgateway.com>
2022-03-29 14:44:26 -0400

    created user show page, separated article partial to use for user show page, indented code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+    def gravatar_for (user, options = {size: 80})
+        email_address = user.email.downcase
+        hash = Digest::MD5.hexdigest(email_address)
+        size = options[:size]
+        gravatar_url = image_src = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
+        image_tag(gravatar_url, alt: user.username, class: "mt-3 rounded-circle shadow")
+    end
 end
