f2cfe15219ef6faa82d9293f022c0c86e1e18b54
nida.ghuman <nidaghuman@nidas-mbp.myfiosgateway.com>
2022-03-30 14:56:54 -0400

    added delete user functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController <  ApplicationController
-    before_action :set_user, only: [:show, :edit, :update]
+    before_action :set_user, only: [:show, :edit, :update, :destroy]
     before_action :require_user, only: [:edit, :update]
-    before_action :require_same_user, only: [:edit, :update]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
 
 def show
     @articles = @user.articles
@@ -33,6 +33,12 @@ def update
     else
         render "edit"
     end
+end
+
+def destroy
+    @user.destroy
+    session[:user_id] = nil
+    redirect_to users_path
 end
 
 private
