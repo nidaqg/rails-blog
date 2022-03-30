47e4bfc6b771b53c1951f85daa65252dc8e59536
nida.ghuman <nidaghuman@nidas-mbp.myfiosgateway.com>
2022-03-30 13:23:04 -0400

    added sessions to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController <  ApplicationController
+    before_action :set_user, only: [:show, :edit, :update]
 def show
-    @user = User.find(params[:id])
     @articles = @user.articles
 end
 def index
@@ -12,6 +12,7 @@ end
 def create
 @user = User.new(user_params)
 if @user.save
+    session[:user_id] = @user.id
     flash[:notice] = "Successful Signup!"
     redirect_to @user
 else
@@ -20,11 +21,9 @@ end
 end
 
 def edit
-    @user = User.find(params[:id])
 end
 
 def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
         flash[:notice] = "Your account was successfuly updated!"
         redirect_to @user
@@ -36,5 +35,8 @@ end
 private
 def user_params
     params.require(:user).permit(:username, :email, :password)
+end
+def set_user
+    @user = User.find(params[:id])
 end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,21 @@
 module ApplicationHelper
+    # helper method for gravatar
     def gravatar_for (user, options = {size: 80})
         email_address = user.email.downcase
         hash = Digest::MD5.hexdigest(email_address)
         size = options[:size]
         gravatar_url = image_src = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
         image_tag(gravatar_url, alt: user.username, class: "mt-3 rounded-circle shadow")
+    end
+
+    #helper method for user sessions
+    def current_user
+        if session[:user_id]
+            @current_user ||= User.find(session[:user_id])
+        end
+    end
+
+    def logged_in?
+        !!current_user
     end
 end
